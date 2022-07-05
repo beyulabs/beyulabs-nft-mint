f0dcd90a1e1cbd1818ae3e9bb91be2d012822eb3
Ryan Harris <harrisryan1@gmail.com>
2022-07-05 13:41:53 -0400

    tweak styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -78,7 +78,7 @@ const Mint: NextPage = () => {
 
       <div className="absolute top-0 left-0 right-0 bottom-0 h-screen w-full opacity-20 bg-black z-30 lg:hidden"></div>
 
-      <section className="h-screen px-2 flex flex-col justify-center items-center mb-8 md:mb-0 text-center lg:text-left lg:p-0 lg:h-fit lg:py-24 lg:mb-48">
+      <section className="h-screen px-2 flex flex-col justify-center items-center mb-8 md:mb-0 text-center lg:text-left lg:p-0 lg:h-fit lg:py-24 lg:mb-8">
         <div className="mt-10 flex flex-col items-center z-30 lg:flex-row lg:items-start lg:mt-0">
           <div className="px-2 md:px-6 lg:w-1/2">
             <h2 className="text-4xl md:text-6xl mb-5 text-white font-medium drop-shadow-2xl">
@@ -102,7 +102,7 @@ const Mint: NextPage = () => {
         </div>
       </section>
 
-      <section className="mb-16 md:px-8 md:bg-[url('/mission-bg.svg')] md:bg-contain md:bg-no-repeat lg:mb-0 lg:pt-12 lg:pb-80 lg:bg-contain">
+      <section className="mb-16 md:px-8 md:bg-[url('/mission-bg.svg')] md:bg-contain md:bg-no-repeat lg:py-56 lg:mb-16 lg:bg-contain lg:bg-center">
         <div className="pb-6 bg-[url('/mobile-mission-bg.svg')] bg-no-repeat bg-center bg-cover relative text-center md:bg-none md:text-left lg:pb-0">
           <div className="w-36 absolute -left-8 -scale-x-100 md:hidden">
             <Image alt="Planet icon" src={planetSVG} layout="responsive" />
@@ -110,12 +110,12 @@ const Mint: NextPage = () => {
           <div className="flex flex-col md:flex-row justify-center items-center md:items-baseline">
             <div className="w-80 md:w-1/3">
               <div className="md:w-full md:relative">
-                <div className="-mt-5 md:absolute md:-top-24 md:mt-0 md:scale-115 lg:-top-24s">
+                <div className="-mt-5 md:absolute md:-top-12 md:-left-6 md:mt-0 lg:scale-05 lg:-top-28">
                   <Image src={nexusMissionImage} alt="Nexus Mission" />
                 </div>
               </div>
             </div>
-            <div className="md:w-2/3 md:ml-2 md:pt-12 md:pr-2">
+            <div className="md:w-2/3 md:ml-6 md:pt-8 md:pr-2 lg:pt-2">
               <h2 className="text-white uppercase font-bold text-3xl md:text-4xl mb-4 px-6 md:px-0">
                 The Nexus Voyagers mission
               </h2>
@@ -217,7 +217,7 @@ const Mint: NextPage = () => {
         </div>
       </section>
 
-      <section className="mb-20 lg:mb-40">
+      <section className="mb-20 lg:mb-36">
         <VoyagerCharacters />
       </section>
 
