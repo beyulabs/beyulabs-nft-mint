561454f1fac1bf20ecd9aa5b55075612dfda4d13
AlexRybakow <sancho725@gmail.com>
2022-07-26 13:39:27 +0300

    edit voyagers carousel

--- components/VoyagerCharacters.tsx
+++ components/VoyagerCharacters.tsx
@@ -1,52 +1,36 @@
 import VoyagerCard from "./VoyagerCard";
 import { voyagers } from "../constants";
 import { Voyager } from "../types/common";
-import Carousel from 'react-multi-carousel';
-
+import { Carousel } from "react-responsive-carousel";
 
 const VoyagerCharacters = () => {
-  const createAvatarCards = voyagers.map((voyager: Voyager) => (
-    <div className="mr-10">
+  const createAvatarCards = () => {
+    return voyagers.map((voyager: Voyager) => (
+      <div className="mr-10">
       <VoyagerCard
         key={`${voyager.nexusName.toLowerCase()}`}
         voyager={voyager}
       />
       </div>
     ));
-;
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 1,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 1,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
-};
+  };
 
   return (
     <div>
       <h2 className="text-6xl text-white font-bold drop-shadow-xl mb-24 text-center uppercase">
         The Voyagers
       </h2>
-      <div className="flex flex-row flex-nowrap justify-center items-start mb-64">
-      {/* <Carousel
-          responsive={responsive}
-          ssr
-          showDots={true}
-          slidesToSlide={1}
-          containerClass={`w-full`}
-          itemClass={`w-voyagerCard h-80 mx-4 relative cursor-pointer`}
-          deviceType={''}
-          centerMode={true}
-        > */}
-        {createAvatarCards}
-        {/* </Carousel> */}
+      <div className="flex flex-row flex-wrap justify-center mb-64">
+      <Carousel
+        autoPlay={false}
+        showStatus={false}
+        showArrows={true}
+        showThumbs={false}
+        centerMode={true}
+        centerSlidePercentage={25} 
+      >
+        {createAvatarCards()}
+      </Carousel>
       </div>
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 import type { AppProps } from "next/app";
 
