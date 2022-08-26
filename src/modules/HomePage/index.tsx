c7a697d9310c951662027c0e9f6b1886a519e654
AlexanderOsadchyy <alex.o@blaize.tech>
2022-08-26 17:52:34 +0300

    Fix durations

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -114,66 +114,66 @@ const HomePage: FC = () => {
             <div className={s.mission__row}>
               <div>
                 {theme === 'light' && (
-                  <Slide left duration={800} ssrFadeout>
-                    <img
-                      src={'/nexus-mission-light.png'}
-                      // width={524}
-                      // height={679}
-                      alt="Nexus Mission"
-                    />
-                  </Slide>
+                  // <Slide left duration={800} ssrFadeout>
+                  <img
+                    src={'/nexus-mission-light.png'}
+                    // width={524}
+                    // height={679}
+                    alt="Nexus Mission"
+                  />
+                  // </Slide>
                 )}
                 {theme === 'dark' && (
-                  <Slide left duration={800} ssrFadeout>
-                    <img
-                      src={'/nexus_mission.png'}
-                      // width={524}
-                      // height={679}
-                      alt="Nexus Mission"
-                    />
-                  </Slide>
+                  // <Slide left duration={800} ssrFadeout>
+                  <img
+                    src={'/nexus_mission.png'}
+                    // width={524}
+                    // height={679}
+                    alt="Nexus Mission"
+                  />
+                  // </Slide>
                 )}
               </div>
-              <Slide right duration={1500} ssrFadeout>
-                <div>
-                  <h2>
-                    The Nexus <br />
-                    Voyagers mission
-                  </h2>
-                  <p>
-                    To terraform Web3. An ecosystem cultivated by developers,
-                    artists, enthusiasts, and investors. A Solarpunk utopia
-                    powered by BeYu Labs and driven by radical optimism and
-                    sustainability.
-                  </p>
-                  <p>
-                    A collection of 10,119 crew members, primed for the genesis
-                    mission launch. On this voyage, crew members will have
-                    access to epic shared experiences, meaningful collabs, one
-                    of a kind NFT drops, and a future built for the community
-                    over time. With the focus on empowering Web3
-                    entrepreneurship the ecosystem will fill the underlying gaps
-                    on contribution with incentivized exchange of utility. Each
-                    of us will have a role to play.
-                  </p>
-                  <p className={s.upper}>
-                    Bridging the gap between dream and reality, virtual and
-                    real, we build a regenerative vision of the future.
-                  </p>
-                  <h4>
-                    Are you mission ready? <a href="#">Join now!</a>
-                  </h4>
-                  <div className={s.button}>
-                    <Button
-                      size="large"
-                      text="Mint"
-                      color="green"
-                      icon={<Coins />}
-                      disabled
-                    />
-                  </div>
+              {/* <Slide right duration={1500} ssrFadeout> */}
+              <div>
+                <h2>
+                  The Nexus <br />
+                  Voyagers mission
+                </h2>
+                <p>
+                  To terraform Web3. An ecosystem cultivated by developers,
+                  artists, enthusiasts, and investors. A Solarpunk utopia
+                  powered by BeYu Labs and driven by radical optimism and
+                  sustainability.
+                </p>
+                <p>
+                  A collection of 10,119 crew members, primed for the genesis
+                  mission launch. On this voyage, crew members will have access
+                  to epic shared experiences, meaningful collabs, one of a kind
+                  NFT drops, and a future built for the community over time.
+                  With the focus on empowering Web3 entrepreneurship the
+                  ecosystem will fill the underlying gaps on contribution with
+                  incentivized exchange of utility. Each of us will have a role
+                  to play.
+                </p>
+                <p className={s.upper}>
+                  Bridging the gap between dream and reality, virtual and real,
+                  we build a regenerative vision of the future.
+                </p>
+                <h4>
+                  Are you mission ready? <a href="#">Join now!</a>
+                </h4>
+                <div className={s.button}>
+                  <Button
+                    size="large"
+                    text="Mint"
+                    color="green"
+                    icon={<Coins />}
+                    disabled
+                  />
                 </div>
-              </Slide>
+              </div>
+              {/* </Slide> */}
             </div>
           </div>
         </section>
