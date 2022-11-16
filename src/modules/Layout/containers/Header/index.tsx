374f2d1722e199bfa94129cf3705cf2ded203a2b
beyulabs.io <98666121+beyulabs@users.noreply.github.com>
2022-11-16 06:13:16 +0530

    Update index.tsx

--- src/modules/Layout/containers/Header/index.tsx
+++ src/modules/Layout/containers/Header/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Link from 'next/link';
 import SocialIcons from '@components/SocalIcons/SocialIcons';
 
